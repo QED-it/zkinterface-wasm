9723c3c49cf59856c4ac47d6c49065e74bc980c6
Aurélien Nicolas <aurel@qed-it.com>
2019-06-10 17:28:08 +0200

    Display the statement and messages on the page

--- www/index.js
+++ www/index.js
@@ -11,12 +11,28 @@ def main(field x, private field y) -> (field):
     return xx + yy
 
 `;
+document.getElementById("program").innerText = code.trim();
+
 let constraints = zkif_zokrates.make_constraint_system(code);
+document.getElementById("cs").innerText = zkif_zokrates.pretty(constraints);
 
 // Prove.
 let x = 3, y = 4;
 let {prover_msg, verifier_msg} = zkif_zokrates.make_witness(code, x, y);
+
 let proof = zkif_bulletproofs.prove(constraints, prover_msg);
+
+document.getElementById("prover").innerText = `
+x = ${x}
+y = ${y}
+
+${zkif_zokrates.pretty(prover_msg)}
+`.trim();
+
+document.getElementById("verifier").innerText = `
+${zkif_zokrates.pretty(verifier_msg)}
+Bulletproof: ${proof.length} bytes
+`.trim();
 
 // …Prover sends verifier_msg and proof to the verifier…
 
@@ -24,14 +40,12 @@ let proof = zkif_bulletproofs.prove(constraints, prover_msg);
 let verif = zkif_bulletproofs.verify(constraints, verifier_msg, proof);
 
 // Display what we verified.
-let public_inputs = zkif_zokrates.parse_verifier_msg(verifier_msg);
-let v_x = public_inputs[1][0];
-let v_zz = public_inputs[2][0];
+function statement(msg) {
+    let public_inputs = zkif_zokrates.parse_verifier_msg(msg);
+    let x = public_inputs[1][0];
+    let zz = public_inputs[2][0];
+    return `${x}^2 + y^2 = ${zz}`
+}
 
-let status = `The statement ${v_x}^2 + y^2 = ${v_zz} is ${verif && "proven" || "NOT proven"}.`;
+let status = `The statement is ${verif && "proven" || "NOT proven"}:  ${statement(verifier_msg)}`;
 document.getElementById("status").innerText = status;
-
-// Display the details of all messages.
-log("Constraint system", zkif_zokrates.pretty(constraints));
-log("Prover", zkif_zokrates.pretty(prover_msg));
-log("Verifier", zkif_zokrates.pretty(verifier_msg));
