ef66b4a2534b01165845ba412eda5ac761c145b3
tronch0 <32448470+tronch0@users.noreply.github.com>
2019-08-04 20:02:50 +0300

    fix(script,makefile): fix prints and order of operations (#9)
    * fix(script,makefile): fix prints and order of operations
    
    * fix: update rustlang version
    
    * fix: downgrade rust version
    
    * add version print
    
    * fix: change version
    
    * remove unnecessary code

--- ci/script.sh
+++ ci/script.sh
@@ -2,21 +2,21 @@
 
 set +x
 
-echo executing scripit.sh
+echo -e "executing scripit.sh"
 
-echo processing bulletproof: checks & tests
+echo -e "processing bulletproof: checks & tests:"
 cd ./zkif-wasm-bundles/zkif-wasm-bulletproofs/
 cargo +nightly-2019-06-22 fmt -- --check
 cargo +nightly-2019-06-22 clippy -- -D warnings
 cargo +nightly-2019-06-22 test
 
-echo processing zk-interface wasm example: checks & tests
+echo -e "processing zk-interface wasm example: checks & tests:"
 cd ../zkif-wasm-example
 cargo +nightly-2019-06-22 fmt -- --check
 cargo +nightly-2019-06-22 clippy -- -D warnings
 cargo +nightly-2019-06-22 test
 
-echo processing zokrates: checks & tests
+echo -e "processing zokrates: checks & tests:"
 cd ../zkif-wasm-zokrates
 cargo +nightly-2019-06-22 fmt -- --check
 cargo +nightly-2019-06-22 clippy -- -D warnings
@@ -24,6 +24,6 @@ cargo +nightly-2019-06-22 test
 
 cd ../../ci
 make
-cd ../demo
+# cd ../demo
 #npm install
 #npm run build
